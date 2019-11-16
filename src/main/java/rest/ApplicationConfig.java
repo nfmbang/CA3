398b9d5227e98b79300538f17654268d3d3fbff1
nfmbang <43202392+nfmbang@users.noreply.github.com>
2019-11-16 18:05:41 +0100

    google-search
    søger custom google søgemaskine

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -25,6 +25,7 @@ public class ApplicationConfig extends Application {
         resources.add(errorhandling.GenericExceptionMapper.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
         resources.add(rest.ExampleResource.class);
+        resources.add(rest.GoogleResource.class);
         resources.add(security.JWTAuthenticationFilter.class);
         resources.add(security.LoginEndpoint.class);
         resources.add(security.RolesAllowedFilter.class);
--- src/main/java/rest/GoogleResource.java
+++ src/main/java/rest/GoogleResource.java
@@ -5,11 +5,23 @@
  */
 package rest;
 
+import DTO.msg;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import errorhandling.ExceptionDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import javax.annotation.security.RolesAllowed;
 
 import utils.ReeQuest;
 import javax.ws.rs.GET;
@@ -29,17 +41,17 @@ public class GoogleResource {
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
     //ID of the custom search engine to use
-    private static final String cx = "003739661964971794239:gvzfdunqtk6";
+    private static final String CX = "003739661964971794239:gvzfdunqtk6";
 
     //API Key
-    private static final String key = "AIzaSyBXm8bLJjCtmNHDZ17xLMzd9rhkpjauPQk";
+    private static final String KEY = "AIzaSyBXm8bLJjCtmNHDZ17xLMzd9rhkpjauPQk";
 
-    private final String URL = "https://www.googleapis.com/customsearch/v1";
+    private final String URL = "https://www.googleapis.com";
+    private final String PATH = "customsearch/v1";
     //    private final String USER_AGENT = "Mozilla/5.0";
 
     @GET
     @Produces({MediaType.APPLICATION_JSON})
-
     public String demo() {
         return "{\"msg\":\"Poormansgoogle API\"}";
     }
@@ -48,14 +60,49 @@ public class GoogleResource {
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     public String search(@PathParam("query") String query) throws Exception {
-        ReeQuest req = new ReeQuest("hvad fuck bruger du source til martin??", URL);
+//        ReeQuest req = new ReeQuest("hvad fuck bruger du source til martin??", URL);
+        String result;
         Map<String, String> params = new HashMap();
 
-        params.put("cx", cx);
-        params.put("key", key);
+        params.put("cx", CX);
+        params.put("key", KEY);
         params.put("q", query);
 
-        return req.getRequest("", params);
+//        return req.getRequest(PATH, params);
+        //return "its a sting: " + query;
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+        BufferedReader in;
+
+        try {
+            URL obj = new URL(URL + "/" + PATH + "?key=" + KEY + "&cx=" + CX + "&q=" + query);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+
+            int responseCode = con.getResponseCode();
+
+            if (responseCode == 200) {
+                in = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+            } else {
+                in = new BufferedReader(new InputStreamReader(con.getErrorStream(), "UTF-8"));
+            }
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            if (responseCode == 200) {
+                result = response.toString();
+                return GSON.toJson(result);
+            } else {
+                ExceptionDTO Err = new ExceptionDTO(responseCode, "fejl" + response.toString());
+                return GSON.toJson(Err);
+            }
+
+        } catch (IOException me) {
+            throw new Exception(me.getMessage());
+        }
     }
 
 }

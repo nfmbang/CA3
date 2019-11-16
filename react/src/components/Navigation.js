4fb2900781fde5ded5a4854d17fb95ec78f5ee93
JonatanHjelm95 <cph-jh409@cphbusiness.dk>
2019-11-16 14:33:12 +0100

    added router to react

---
+++ react/src/components/Navigation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
+const Navigation = () => {
+    return (
+        <ul class="header">
+            <li>
+                <NavLink to="/" exact>Home</NavLink>
+            </li>
+            <li>
+                <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+                <NavLink to="/contact">Contact</NavLink>
+            </li>
+        </ul>
+    );
+}
+
+export default Navigation;
